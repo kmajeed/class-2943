56338d48023e4f5914d0da13bae8eabb520fcd33
Marko Gargenta <marko@marakana.com>
2013-02-12 14:05:41 -0800

    Implemented onClickListener

--- Yamba/src/com/control4/yamba/StatusActivity.java
+++ Yamba/src/com/control4/yamba/StatusActivity.java
@@ -1,22 +1,32 @@
 package com.control4.yamba;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class StatusActivity extends Activity {
+public class StatusActivity extends Activity implements OnClickListener {
+	private Button buttonUpdate;
+	private EditText editStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
+        
+        editStatus = (EditText) findViewById(R.id.edit_status);
+        buttonUpdate = (Button) findViewById(R.id.button_update);
+        buttonUpdate.setOnClickListener( this );
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_status, menu);
-        return true;
-    }
+	@Override
+	public void onClick(View v) {
+		String status = editStatus.getText().toString();
+		
+		Log.d("Yamba", "onClicked with text: "+status);
+	}
     
 }
