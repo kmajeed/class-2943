cdc2011b56272f5f91582fdd2f74060ea027b95e
Marko Gargenta <marko@marakana.com>
2013-02-12 14:24:27 -0800

    Refactored to use onClick binding

--- Yamba/src/com/control4/yamba/StatusActivity.java
+++ Yamba/src/com/control4/yamba/StatusActivity.java
@@ -4,12 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.EditText;
 
-public class StatusActivity extends Activity implements OnClickListener {
-	private Button buttonUpdate;
+public class StatusActivity extends Activity {
 	private EditText editStatus;
 
     @Override
@@ -18,11 +15,8 @@ public class StatusActivity extends Activity implements OnClickListener {
         setContentView(R.layout.activity_status);
         
         editStatus = (EditText) findViewById(R.id.edit_status);
-        buttonUpdate = (Button) findViewById(R.id.button_update);
-        buttonUpdate.setOnClickListener( this );
     }
 
-	@Override
 	public void onClick(View v) {
 		String status = editStatus.getText().toString();
 		
