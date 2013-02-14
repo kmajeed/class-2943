51266946ca36dedeeea1ac84e6d389d0baccceb0
Marko Gargenta <marko@marakana.com>
2013-02-14 10:13:25 -0800

    Implemented ViewBinder

--- Yamba/src/com/control4/yamba/TimelineFragment.java
+++ Yamba/src/com/control4/yamba/TimelineFragment.java
@@ -6,24 +6,45 @@ import android.content.CursorLoader;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.text.format.DateUtils;
+import android.view.View;
 import android.widget.SimpleCursorAdapter;
+import android.widget.SimpleCursorAdapter.ViewBinder;
+import android.widget.TextView;
 
 public class TimelineFragment extends ListFragment implements
 		LoaderCallbacks<Cursor> {
 	private static final String[] FROM = { StatusContract.Columns.USER,
-			StatusContract.Columns.MESSAGE };
-	private static final int[] TO = { android.R.id.text1, android.R.id.text2 };
+			StatusContract.Columns.MESSAGE, StatusContract.Columns.CREATED_AT };
+	private static final int[] TO = { R.id.text_user, R.id.text_message,
+			R.id.text_created_at };
 	private static final int LOADER_ID = 42;
 	private SimpleCursorAdapter adapter;
-	
+
+	private static final ViewBinder VIEW_BINDER = new ViewBinder() {
+		@Override
+		public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+			if (view.getId() != R.id.text_created_at)
+				return false;
+			
+			// Custom binding
+			long timestamp = cursor.getLong(columnIndex);
+			CharSequence relTime = DateUtils.getRelativeTimeSpanString(timestamp);
+			((TextView)view).setText(relTime);
+			
+			return true;
+		}
+	};
+
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 
 		setEmptyText("Loading...");
-		
-		adapter = new SimpleCursorAdapter(getActivity(),
-				android.R.layout.simple_list_item_2, null, FROM, TO, 0);
+
+		adapter = new SimpleCursorAdapter(getActivity(), R.layout.row, null,
+				FROM, TO, 0);
+		adapter.setViewBinder(VIEW_BINDER);
 
 		setListAdapter(adapter);
 
@@ -35,7 +56,8 @@ public class TimelineFragment extends ListFragment implements
 	/** Executes on a non-UI thread. */
 	@Override
 	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-		if(id!=LOADER_ID) return null;
+		if (id != LOADER_ID)
+			return null;
 		return new CursorLoader(getActivity(), StatusContract.CONTENT_URI,
 				null, null, null, StatusContract.DEFAULT_SORT);
 	}
