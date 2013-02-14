83fb8ee3cb2e2d20aca6e1ae0efc6d47b2a50c08
Marko Gargenta <marko@marakana.com>
2013-02-13 16:03:54 -0800

    Refactored StatusActivity to StatusFragment

--- Yamba/src/com/control4/yamba/StatusActivity.java
+++ Yamba/src/com/control4/yamba/StatusActivity.java
@@ -1,99 +1,14 @@
 package com.control4.yamba;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
-import android.graphics.Color;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.marakana.android.yamba.clientlib.YambaClient;
-import com.marakana.android.yamba.clientlib.YambaClientException;
-
-public class StatusActivity extends Activity implements TextWatcher {
-	private static final int MAX_COUNT = 140;
-	private EditText editStatus;
-	private TextView textCount;
-	private int defaultTextColor;
+public class StatusActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_status);
-
-		textCount = (TextView) findViewById(R.id.text_count);
-		textCount.setText(String.valueOf(MAX_COUNT));
-		defaultTextColor = textCount.getTextColors().getDefaultColor();
-
-		editStatus = (EditText) findViewById(R.id.edit_status);
-		editStatus.addTextChangedListener(this);
 	}
-
-	public void onClick(View v) {
-		String status = editStatus.getText().toString();
-		new PostStatusTask().execute(status);
-	}
-
-	class PostStatusTask extends AsyncTask<String, Void, String> {
-		ProgressDialog dialog;
-
-		@Override
-		protected void onPreExecute() {
-			dialog = ProgressDialog.show(StatusActivity.this, null,
-					"Posting...please wait");
-		}
-
-		/** Executes on a separate worker thread. */
-		@Override
-		protected String doInBackground(String... params) {
-			try {
-				YambaClient yamba = new YambaClient("student", "password");
-				yamba.postStatus(params[0]);
-				Log.d("Yamba", "onClicked with text: " + params[0]);
-				return "Successfully posted";
-			} catch (YambaClientException e) {
-				Log.e("Yamba", "Failed to post", e);
-				e.printStackTrace();
-				return "Failed to post";
-			}
-		}
-
-		/** Executes on UI thread after doInBackground() is done. */
-		@Override
-		protected void onPostExecute(String result) {
-			dialog.dismiss();
-			Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG)
-					.show();
-		}
-
-	}
-
-	// --- TextWatcher Callbacks ---
-	@Override
-	public void afterTextChanged(Editable s) {
-		int count = 140 - s.length();
-		textCount.setText(String.valueOf(count));
-
-		if (count < 50) {
-			textCount.setTextColor(Color.RED);
-		} else {
-			textCount.setTextColor(defaultTextColor);
-		}
-	}
-
-	@Override
-	public void beforeTextChanged(CharSequence s, int start, int count,
-			int after) {
-	}
-
-	@Override
-	public void onTextChanged(CharSequence s, int start, int before, int count) {
-	}
-
+	
 }
---
+++ Yamba/src/com/control4/yamba/StatusFragment.java
@@ -0,0 +1,111 @@
+package com.control4.yamba;
+
+import android.app.Fragment;
+import android.app.ProgressDialog;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.marakana.android.yamba.clientlib.YambaClient;
+import com.marakana.android.yamba.clientlib.YambaClientException;
+
+public class StatusFragment extends Fragment implements TextWatcher, OnClickListener {
+
+	private static final int MAX_COUNT = 140;
+	private Button buttonUpdate;
+	private EditText editStatus;
+	private TextView textCount;
+	private int defaultTextColor;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater
+				.inflate(R.layout.fragment_status, container, false);
+
+		buttonUpdate = (Button) view.findViewById(R.id.button_update);
+		buttonUpdate.setOnClickListener(this);
+		textCount = (TextView) view.findViewById(R.id.text_count);
+		textCount.setText(String.valueOf(MAX_COUNT));
+		defaultTextColor = textCount.getTextColors().getDefaultColor();
+
+		editStatus = (EditText) view.findViewById(R.id.edit_status);
+		editStatus.addTextChangedListener(this);
+
+		return view;
+	}
+
+	@Override
+	public void onClick(View v) {
+		String status = editStatus.getText().toString();
+		new PostStatusTask().execute(status);
+	}
+
+	class PostStatusTask extends AsyncTask<String, Void, String> {
+		ProgressDialog dialog;
+
+		@Override
+		protected void onPreExecute() {
+			dialog = ProgressDialog.show(getActivity(), null,
+					"Posting...please wait");
+		}
+
+		/** Executes on a separate worker thread. */
+		@Override
+		protected String doInBackground(String... params) {
+			try {
+				YambaClient yamba = new YambaClient("student", "password");
+				yamba.postStatus(params[0]);
+				Log.d("Yamba", "onClicked with text: " + params[0]);
+				return "Successfully posted";
+			} catch (YambaClientException e) {
+				Log.e("Yamba", "Failed to post", e);
+				e.printStackTrace();
+				return "Failed to post";
+			}
+		}
+
+		/** Executes on UI thread after doInBackground() is done. */
+		@Override
+		protected void onPostExecute(String result) {
+			dialog.dismiss();
+			Toast.makeText(getActivity(), result, Toast.LENGTH_LONG)
+					.show();
+		}
+
+	}
+
+	// --- TextWatcher Callbacks ---
+	@Override
+	public void afterTextChanged(Editable s) {
+		int count = 140 - s.length();
+		textCount.setText(String.valueOf(count));
+
+		if (count < 50) {
+			textCount.setTextColor(Color.RED);
+		} else {
+			textCount.setTextColor(defaultTextColor);
+		}
+	}
+
+	@Override
+	public void beforeTextChanged(CharSequence s, int start, int count,
+			int after) {
+	}
+
+	@Override
+	public void onTextChanged(CharSequence s, int start, int before, int count) {
+	}
+
+}
