16809cfe647b73f2d4445c67f022a3a5fba8f584
Marko Gargenta <marko@marakana.com>
2013-02-14 09:19:50 -0800

    Refactored to use Loader

--- Yamba/src/com/control4/yamba/TimelineFragment.java
+++ Yamba/src/com/control4/yamba/TimelineFragment.java
@@ -1,27 +1,49 @@
 package com.control4.yamba;
 
 import android.app.ListFragment;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.CursorLoader;
+import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.widget.SimpleCursorAdapter;
 
-public class TimelineFragment extends ListFragment {
+public class TimelineFragment extends ListFragment implements
+		LoaderCallbacks<Cursor> {
 	private static final String[] FROM = { StatusContract.Columns.USER,
 			StatusContract.Columns.MESSAGE };
 	private static final int[] TO = { android.R.id.text1, android.R.id.text2 };
-
+	private static final int LOADER_ID = 42;
+	private SimpleCursorAdapter adapter;
+	
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 
-		Cursor cursor = getActivity().getContentResolver().query(
-				StatusContract.CONTENT_URI, null, null, null,
-				StatusContract.DEFAULT_SORT);
-
-		SimpleCursorAdapter adapter = new SimpleCursorAdapter(getActivity(),
-				android.R.layout.simple_list_item_2, cursor, FROM, TO,
-				SimpleCursorAdapter.FLAG_AUTO_REQUERY);
+		adapter = new SimpleCursorAdapter(getActivity(),
+				android.R.layout.simple_list_item_2, null, FROM, TO, 0);
 
 		setListAdapter(adapter);
+
+		getLoaderManager().initLoader(LOADER_ID, null, this);
+	}
+
+	// --- Loader Callbacks ---
+
+	/** Executes on a non-UI thread. */
+	@Override
+	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+		return new CursorLoader(getActivity(), StatusContract.CONTENT_URI,
+				null, null, null, StatusContract.DEFAULT_SORT);
+	}
+
+	@Override
+	public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+		adapter.swapCursor(cursor);
+	}
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> loader) {
+		adapter.swapCursor(null);
 	}
 }
